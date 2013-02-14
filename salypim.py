0f0866ea513f2ed5c070869edc7bd39af7a93b51
Rene <rene.camacho.m@gmail.com>
2013-02-14 11:40:43 -0600

    Update salypim.py

--- salypim.py
+++ salypim.py
@@ -1,40 +1,45 @@
-
 import Tkinter
 import random
 import Image, ImageTk
-
-from Tkinter import * 
 from sys import argv
 
-cargar = argv[1] #argumento para cargar imagen 
+cargar = argv[1]
 
-def function(imagen): #creamos ventana 
+polaridad = 1
+intensidad = 1
+
+#Ventana
+def funcion(imagen):
 	imagen_carga = Image.open(imagen)
 	root = Tkinter.Tk()
-	root.title("Sal y Pimienta")	
-	tkimage=ImageTk.PhotoImage(imagen_carga)
-	imagen_carga.save("sal_pim.png")
-	Tkinter.Label(root, image=tkimage).pack()
+	root.title("Sal y pimienta")
+	imagen_carga = salpim(imagen_carga, 0.5, 0)
+	tkimage = ImageTk.PhotoImage(imagen_carga)
+	imagen_carga.save("sp1.png")
+	Tkinter.Label(root, image = tkimage).pack()
 	root.mainloop()
-
-def sal_pim(imagen = cargar): #sal y pimienta 
+	
+#Aplicamos sal y pimienta
+def salpim(imagen, intensidad, polaridad):
 	pixeles = imagen.load()
 	x,y = imagen.size
-	promedio = x * y
-	promedio = 1*(n/100)
+	total = x * y
+	total = int(intensidad*(total/100))
 	i = 0
-	while i != promedio:
-		a,b = random.randint(0, x - 1), random.randint(0, y - 1)
-		det = random.randint(0,1)
-		if det == 1:
-			s = random.randint(255 - 1, 255) 
-			pixeles[a,b] = (s, s, s)
+	print total
+	while i!= total:
+		a, b = random.randint(0, x - 1), random.randint(0, y - 1)
+		vec = random.randint(0,1)
+		if vec == 1:
+			s = random.randint(255 - polaridad, 255)
+			pixeles[a,b] = (s,s,s)
 		else:
-			p = random.randint(0, 1)
-			pixeles[a,b] = (p, p, p)
+			p = random.randint(0, polaridad)
+			pixeles[a,b] = (p,p,p)
 		i += 1
-	
+	return imagen
+
 def main():
-	function(cargar)
-	
+	funcion(cargar)
+
 main()
